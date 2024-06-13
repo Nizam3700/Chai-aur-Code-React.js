448e26b44230e32790e33cc2dba9a84533b05609
Nizamuddin <nizamuddin8637@gmail.com>
2024-06-13 15:09:59 +0530

    PopUp added

--- my-project/src/Layout.jsx
+++ my-project/src/Layout.jsx
@@ -3,13 +3,16 @@ import Head from './Head/Head'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import Pop from './pop/Pop'
 
 function Layout() {
+  
   return (
     <>
     <Head/>
     <Header/>
     <Outlet/>
+    <Pop/>
     <Footer/>
     </>
   )
